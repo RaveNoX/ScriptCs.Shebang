943819a034c20d71de03d64c3a06d7f668ff8e55
Artur Kraev <ravenox@gmail.com>
2014-12-11 20:41:25 +0300

    Initial commit

---
+++ src/ScriptCs.ShebangModule/ShebangLineProcessor.cs
@@ -0,0 +1,22 @@
+﻿#region Usings
+
+using ScriptCs.Contracts;
+
+#endregion
+
+namespace ScriptCs.ShebangModule
+{
+    public class ShebangLineProcessor : ILineProcessor
+    {
+        public bool ProcessLine(IFileParser parser, FileParserContext context, string line, bool isBeforeCode)
+        {            
+            return !isBeforeCode && IsShebang(line);
+        }
+
+        private bool IsShebang(string line)
+        {
+            if (string.IsNullOrWhiteSpace(line)) return false;
+            return line.StartsWith("#!") && line.Length > 2;
+        }
+    }
+}
---
+++ src/ScriptCs.ShebangModule/ShebangModule.cs
@@ -0,0 +1,18 @@
+﻿#region Usings
+
+using System;
+using ScriptCs.Contracts;
+
+#endregion
+
+namespace ScriptCs.ShebangModule
+{
+    [Module("shebang", Autoload = true)]
+    public class ShebangModule : IModule
+    {
+        public void Initialize(IModuleConfiguration config)
+        {            
+            config.LineProcessor<ShebangLineProcessor>();            
+        }
+    }
+}
